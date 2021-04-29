d9ffb269ba099f2d2d2b64ecbb22344428965a9d
Nahuel Christian Fernandez <nahuefer105@outlook.com>
2021-04-29 00:24:01 -0300

    Added meta descriptiong

--- view/projects.view.php
+++ view/projects.view.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <meta name="description" content="Nahuel Fernandez | Portfolio">
+    
     <link rel="stylesheet" href="css/index.css">
     <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
 
